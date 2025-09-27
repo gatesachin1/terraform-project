7d260eda8390a18a078ab5f66eda57210c34499f
Sachin Gate <gatesachin1112@gmail.com>
2025-09-27 23:20:56 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "tf_instance" {
+    ami             = "ami-08982f1c5bf93d976"
+    instance_type   = "t2.micro"
+    key_name        = "N.Virginia-Linux-key-pair"
+
+    tags = {
+        Name  = "Dev-Instance"
+        Owner = "Sachin"
+    }
+}
